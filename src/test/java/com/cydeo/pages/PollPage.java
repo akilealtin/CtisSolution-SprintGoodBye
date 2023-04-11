ad53da9ef32f2262c3067d2289275c9e0a5afc0d
Hasan Demirtas <108940360+Hsande@users.noreply.github.com>
2023-04-11 09:07:33 +0300

    Completed the poll feature

---
+++ src/test/java/com/cydeo/pages/PollPage.java
@@ -0,0 +1,131 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class PollPage extends BasePage {
+
+
+    @FindBy(xpath = "//span[.='Poll']")
+    public WebElement pollButton;
+
+    @FindBy(xpath = "//iframe[@class='bx-editor-iframe']")
+    public WebElement iFrame;
+
+
+    @FindBy(css = "body[contenteditable='true']")
+    public WebElement textBox;
+
+    @FindBy(css = "a#bx-destination-tag")
+    public WebElement addMoreButton;
+
+    @FindBy(xpath = "//a[.='Employees and departments']")
+    public WebElement employeesDepartmentsLink;
+
+    @FindBy(xpath = "//*[@id=\"bx-lm-category-relation-129\"]/a[123]/div[1]/div[1]")
+    public WebElement contact1;
+
+    @FindBy(xpath = "//*[@id=\"bx-lm-category-relation-129\"]/a[1]/div[1]/div[1]")
+    public WebElement contact2;
+
+    @FindBy(xpath = "//*[@id=\"bx-lm-category-relation-129\"]/a[3]/div[1]/div[1]")
+    public WebElement contact3;
+
+    @FindBy(xpath = "//span[@class='popup-window-close-icon']")
+    public WebElement close;
+
+    @FindBy(xpath = "//div[@id='feed-add-post-destination-container']/span/span[2]/span[.='marketing17@cybertekschool.com']")
+    public WebElement verifyContact1;
+
+    @FindBy(xpath = "//div[@id='feed-add-post-destination-container']/span/span[3]/span[.='helpdesk1@cybertekschool.com']")
+    public WebElement verifyContact2;
+    @FindBy(xpath = "//div[@id='feed-add-post-destination-container']/span/span[4]/span[.='hr76@cybertekschool.com']")
+    public WebElement verifyContact3;
+
+
+    @FindBy(xpath = "//input[@placeholder='Question ']")
+    public WebElement questionBox;
+
+
+    @FindBy(xpath = "//input[@placeholder='Answer  1']")
+    public WebElement answerBox1;
+    @FindBy(xpath = "//input[@placeholder='Answer  2']")
+    public WebElement answerBox2;
+    @FindBy(xpath = "//input[@placeholder='Answer  3']")
+    public WebElement answerBox3;
+
+
+    @FindBy(xpath = "(//label[.='Select A']/../label)[1]/span")
+    public WebElement checkbox1;
+
+    @FindBy(xpath = "(//label[.='Select B']/../label)[1]/span")
+    public WebElement checkbox2;
+
+    @FindBy(xpath = "(//label[.='Select C']/../label)[1]/span")
+    public WebElement checkbox3;
+
+
+    @FindBy(id = "multi_1")
+    public WebElement multipleCheckbox;
+
+    @FindBy(id = "blog-submit-button-save")
+    public WebElement sendButton;
+
+    @FindBy(xpath = "(//label[@title='Delete answer'])[3]")
+    public WebElement deleteAnswer;
+
+    @FindBy(xpath = "//label[@title='Delete question']")
+    public WebElement deleteQuestion;
+
+    @FindBy(xpath = "//a[.='Add question']")
+    public WebElement addQuestion;
+
+
+    @FindBy(xpath = "(//button[@data-bx-vote-button='actVoting'])[1]")
+    public WebElement voteButton;
+
+    @FindBy(xpath = "//label[.='Select A']/../../../following-sibling::td[2]/span[.='100%']")
+    public WebElement verifyMultiple1;
+
+    @FindBy(xpath = "//label[.='Select B']/../../../following-sibling::td[2]/span[.='100%']")
+    public WebElement verifyMultiple2;
+
+    @FindBy(xpath = "//label[.='Select C']/../../../following-sibling::td[2]/span[.='100%']")
+    public WebElement verifyMultiple3;
+
+    @FindBy(xpath = "(//span[.='More' and @class='feed-post-more-text' ])[1]")
+    public WebElement moreButton;
+
+    @FindBy(xpath = "//span[.='Delete' and @class='menu-popup-item menu-popup-no-icon ']")
+    public WebElement deleteButton;
+
+    @FindBy(xpath = "//span[.='The post has been deleted.' and @class='feed-add-info-text']")
+    public WebElement deletedMessage;
+
+    @FindBy(xpath = "//span[.='The message title is not specified']")
+    public WebElement titleWarningMessage;
+
+    @FindBy(xpath = "//span[.='The question text is not specified.']")
+    public WebElement questionWarningMessage;
+
+    @FindBy(xpath = "//span[.='Please specify at least one person.']")
+    public WebElement atLeastTwoWarningMessage;
+
+    @FindBy(xpath = "//span[@class='feed-add-post-del-but']")
+    public WebElement deleteAllEmployees;
+
+
+
+    public WebElement verifyAnswerWarningMessage(String question) {
+        String xpath = "//span[.='The question \"" + question + "\" has no answers.']";
+        WebElement answerWarning = Driver.getDriver().findElement(By.xpath(xpath));
+
+        return answerWarning;
+    }
+
+
+}
---
+++ src/test/resources/features/poll.feature
@@ -0,0 +1,69 @@
+
+@CS10-326
+Feature: As a user, I should be able to create a poll by clicking on Poll tab under Active Stream.
+
+  Background: User should be logged in active stream page
+    Given User is on the login page
+    Given user should be login
+
+@CS10-333
+  Scenario: User should be able to add users by selecting multiple contacts from Employees and Department's contact lists and add questions and multiple answers and provide multiple choice to attendees by selecting the Allow multiple choice checkbox and delete questions and multiple answers.
+    When user clicks on the poll button
+    And user type a title as "title"
+    And user clicks on add more link
+    And user clicks on Employees and departments link
+    And user add more than one contacts
+    And user click on the close sign
+    Then verify the added contacts are displayed on the To box
+    When user clicks on Question box
+    And user adds question as "question"
+  And user deletes the question
+  And user clicks add question button
+  And user adds question as "new question"
+    And user clicks on the first Answer box and adds answer as "Select A"
+    And user clicks on the second Answer  box and adds answer as "Select B"
+    And user clicks on the third Answer box and adds answer as "Select C"
+    And user deletes the third Answer
+    And user clicks on Allow multiple choice checkbox
+    And user clicks on send button
+    And user clicks on checkboxes
+    And user clicks on vote button
+    Then verify the added answers are selected as multiple
+    When user clicks on more button
+    And user selects delete
+    And user accepts the popup
+    Then the post should be deleted and the message should be displayed
+
+
+  @CS10-334
+  Scenario: User should be able to create a poll with title.
+    When user clicks on the poll button
+    And user clicks on send button
+    Then the warning message should be displayed about title
+
+  @CS10-335
+  Scenario: User should be able to create a poll with answers.
+    When user clicks on the poll button
+    And user type a title as "title_1"
+    And user clicks on Question box
+    And user adds question as "question1"
+    And user clicks on send button
+    Then the warning message of "question1" should be displayed about answers
+  @CS10-336
+  Scenario: User should be able to create a poll with question.
+    When user clicks on the poll button
+    And user type a title as "title_2"
+    And user clicks on the first Answer box and adds answer as "Select A2"
+    And user clicks on send button
+    Then the warning message should be displayed about question
+
+@CS10-337
+  Scenario: User should be able to create a poll with person.
+    When user clicks on the poll button
+    And user type a title as "title"
+    And user deletes everybody in To box
+    And user clicks on Question box
+    And user adds question as "question"
+    And user clicks on the first Answer box and adds answer as "Select A"
+    And user clicks on send button
+    Then the warning message should be displayed about at least one person
